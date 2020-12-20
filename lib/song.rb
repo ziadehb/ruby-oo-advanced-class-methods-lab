60c292790d89fcdf40e8564192266c1f07784e2d
ziadehb <40444656+ziadehb@users.noreply.github.com>
2020-12-20 11:54:08 -0500

    Add files via upload

--- lib/song.rb
+++ lib/song.rb
@@ -10,4 +10,57 @@ class Song
     self.class.all << self
   end
 
+  def self.create
+    song = Song.new
+    song.save
+    song
+  end
+
+  def self.new_by_name(name)
+    song = self.new
+    song.name = name
+    song
+  end
+
+  def self.create_by_name(name)
+    song = self.create
+    song.name = name
+    song
+  end
+
+  def self.find_by_name(name)
+    self.all.find {|song| song.name == name} 
+  end
+
+  def self.find_or_create_by_name(name)
+    self.find_by_name(name) || self.create_by_name(name)
+  end
+
+  def self.alphabetical
+    self.all.sort_by{|song| song.name}
+  end
+
+  def self.new_from_filename(filename)
+    songs = filename.split(" - ")
+    song = self.new
+    song.name = songs[1].delete_suffix(".mp3")
+    song.artist_name = songs[0]
+    song
+  end 
+
+  def self.destroy_all
+    self.all.clear
+  end
+
+  def self.create_from_filename(filename)
+    songs = filename.split(" - ")
+    song = self.new
+    song.name = songs[1].delete_suffix(".mp3")
+    song.artist_name = songs[0]
+    song.save
+  end 
+   
+  def self.destroy_all
+    self.all.clear
+  end
 end
